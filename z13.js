05778a689c1249eb75579acb048cacd2382403b9
Рената Филиппова <githubddz@gmail.com>
2025-09-16 11:40:16 +0300

    add thirteenth task

---
+++ z13.js
@@ -0,0 +1,9 @@
+function hexStringToRGB(hexString) {
+  const hex = hexString.replace(/^#/, '').toLowerCase();
+
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+
+  return { r, g, b };
+}
