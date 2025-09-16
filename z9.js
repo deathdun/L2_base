c25b69d72ff0680e699c3b7cb01e28b2b211d19c
Рената Филиппова <githubddz@gmail.com>
2025-09-16 11:40:09 +0300

    add ninth task

---
+++ z9.js
@@ -0,0 +1,12 @@
+function solution(str){
+   const result = [];
+  for (let i = 0; i < str.length; i += 2) {
+    
+    if (i + 1 < str.length) {
+      result.push(str[i] + str[i + 1]);
+    } else {
+      result.push(str[i] + '_');
+    }
+  }
+  return result;
+}
